025c7956823776234571bb0f8b54ca56256141d0
Filip Tammergård <filip@tammergard.se>
2022-04-22 13:02:12 +0200

    chore: add jsdocs to useDisclosure

--- src/hooks/useDisclosure/useDisclosure.ts
+++ src/hooks/useDisclosure/useDisclosure.ts
@@ -1,9 +1,14 @@
 import { useState } from "react"
 
-export function useDisclosure(
+/**
+ * Manages boolean state. It provides open, close and toggle handlers and accepts optional onOpen and onClose callbacks. It can be used to manage controlled modals, popovers and other similar components.
+ * @param initialState Controls whether the component's initial state is open or closed.
+ * @param callbacks Contains onOpen and onClose callbacks that are called when the open or close handlers are called.
+ */
+export const useDisclosure = (
   initialState: boolean,
   callbacks?: { onOpen?: () => void; onClose?: () => void },
-): UseDisclosureReturn {
+): UseDisclosureReturn => {
   const [isOpen, setIsOpen] = useState(initialState)
 
   const open = (): void => {
