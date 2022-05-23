bdc5996192c65f59105b4f07ba1451bc9f9e9671
Filip Tammergård <filip@tammergard.se>
2022-05-23 08:15:51 +0200

    chore: add jsdoc to usePrefersColorScheme

--- src/hooks/usePrefersColorScheme/usePrefersColorScheme.ts
+++ src/hooks/usePrefersColorScheme/usePrefersColorScheme.ts
@@ -1,5 +1,8 @@
 import { useMediaQuery } from "../useMediaQuery/useMediaQuery"
 
+/**
+ * Subscribes to color scheme preference changes and returns dark if dark scheme is preferred and light if light scheme is preferred.
+ */
 export const usePrefersColorScheme = (): "dark" | "light" => {
   return useMediaQuery("(prefers-color-scheme: dark)") ? "dark" : "light"
 }
