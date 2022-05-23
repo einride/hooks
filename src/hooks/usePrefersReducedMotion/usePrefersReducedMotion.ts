286a590b52911ff7269ff0c8d1fc9987aa678f86
Filip Tammergård <filip@tammergard.se>
2022-05-23 08:15:39 +0200

    chore: add jsdoc to usePrefersReducedMotion

--- src/hooks/usePrefersReducedMotion/usePrefersReducedMotion.ts
+++ src/hooks/usePrefersReducedMotion/usePrefersReducedMotion.ts
@@ -1,5 +1,8 @@
 import { useMediaQuery } from "../useMediaQuery/useMediaQuery"
 
+/**
+ * Subscribes to reduced motion preference changes and returns true if reduced motion is preferred and false if not.
+ */
 export const usePrefersReducedMotion = (): boolean => {
   return useMediaQuery("(prefers-reduced-motion: reduce)")
 }
