f093dfefee0631711842602f0660e2359eaed65a
quy-einride <quy.truong@einride.tech>
2022-08-22 14:37:38 +0200

    feat: output both cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,15 @@ import typescript from "@rollup/plugin-typescript"
 export default {
   external: ["react"],
   input: "src/main.ts",
-  output: {
-    dir: "dist",
-    format: "esm",
-  },
+  output: [
+    {
+      file: "dist/cjs/main.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/esm/main.js",
+      format: "esm",
+    },
+  ],
   plugins: [typescript({ tsconfig: "./tsconfig.build.json" })],
 }
