81d0e968ed6f1376f37ca397247ba19ef7318a16
Filip Tammergård <filip@tammergard.se>
2023-01-23 13:30:22 +0100

    fix: add custom rollup config

---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+import typescript from "@rollup/plugin-typescript"
+import pkg from "./package.json" assert { type: "json" }
+
+export default {
+  input: "src/main.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies ?? {}), ...Object.keys(pkg.peerDependencies ?? {})],
+  plugins: [typescript({ tsconfig: "./tsconfig.build.json" })],
+}
