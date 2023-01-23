93f127ea9b6fc11da1c81c85a07429294255901f
Filip Tammergård <filip@tammergard.se>
2023-01-23 13:36:11 +0100

    fix: use custom rollup config until vite lib mode problem is resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { resolve } from "path"
-import typescript from "@rollup/plugin-typescript"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import pkg from "./package.json"
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: [...Object.keys(pkg.peerDependencies ?? {})],
-      plugins: [typescript({ tsconfig: resolve(__dirname, "./tsconfig.build.json") })],
-    },
-  },
   plugins: [react()],
   test: {
     globals: true,
